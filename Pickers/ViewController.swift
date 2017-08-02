338689db838948b8684c05c43989b3328fbd74c1
Pirokar <emessage@yandex.ru>
2017-08-02 11:50:17 +0300

    Initial Commit

---
+++ Pickers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pickers
+//
+//  Created by Владимир Рыбалка on 02/08/2017.
+//  Copyright © 2017 Vladimir Rybalka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
