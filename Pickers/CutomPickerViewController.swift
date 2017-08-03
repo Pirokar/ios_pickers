4c32b7f6b1ece3ba59ec284ecac9d795e812a14e
Vladimir Rybalka <vladimir@Vladimirs-Mac.local>
2017-08-03 12:47:41 +0300

    Created game tab

--- Pickers/CutomPickerViewController.swift
+++ Pickers/CutomPickerViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import AudioToolbox
 
-class CutomPickerViewController: UIViewController {
+class CutomPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    private var images: [UIImage]!
+    private var winSoundID: SystemSoundID = 0
+    private var crunchSoundID: SystemSoundID = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        images = [
+            UIImage(named: "seven")!,
+            UIImage(named: "bar")!,
+            UIImage(named: "crown")!,
+            UIImage(named: "cherry")!,
+            UIImage(named: "lemon")!,
+            UIImage(named: "apple")!
+        ]
+        winLabel.text = " "
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +37,81 @@ class CutomPickerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func showButton() {
+        button.isHidden = false
     }
-    */
+    
+    func playWinSound() {
+        if(winSoundID == 0) {
+            let soundPath = Bundle.main.url(forResource: "win", withExtension: "wav")
+            AudioServicesCreateSystemSoundID(soundPath! as CFURL, &winSoundID)
+        }
+        AudioServicesPlaySystemSound(winSoundID)
+        winLabel.text = "ПОБЕДИТЕЛЬ!"
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.showButton()
+        }
+    }
+
+    @IBAction func spin(_ sender: Any) {
+        var win = false
+        var numInRow = -1
+        var lastVal = -1
+        
+        for i in 0..<5 {
+            if(crunchSoundID == 0) {
+                let soundPath = Bundle.main.url(forResource: "crunch", withExtension: "wav")
+                AudioServicesCreateSystemSoundID(soundPath! as CFURL, &crunchSoundID)
+            }
+            AudioServicesPlaySystemSound(crunchSoundID)
+            
+            let newValue = Int(arc4random_uniform(UInt32(images.count)))
+            if (newValue == lastVal) {
+                numInRow += 1
+            } else {
+                numInRow = 1
+            }
+            lastVal = newValue
+            
+            picker.selectRow(newValue, inComponent: i, animated: true)
+            picker.reloadComponent(i)
+            if(numInRow >= 3) {
+                win = true
+            }
+        }
+        
+        if(win) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.playWinSound()
+            }
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.showButton()
+            }
+        }
+        button.isHidden = true
+        winLabel.text = " "
+    }
+    
+    // MARK: Picker data source methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return images.count
+    }
+    
+    // MARK: Picker delegate methods
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let image = images[row]
+        let imageView = UIImageView(image: image)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 64
+    }
 
 }
